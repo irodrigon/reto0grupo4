56502074a40bdbfc47bdbe6a09e6a3618e05705b
irodrigon <irodrigon2313@gmail.com>
2024-09-17 09:34:27 +0200

    filled class enunciado

--- Reto0Grupo4ADTi/src/model/Enunciado.java
+++ Reto0Grupo4ADTi/src/model/Enunciado.java
@@ -11,4 +11,63 @@ package model;
  */
 public class Enunciado {
     
+    private Integer idE;
+    private String descripcion;
+    private Dificultad nivel;
+    private boolean disponible;
+    private String ruta;
+    
+    public Enunciado(){
+    
+    }
+
+    public Enunciado(Integer id, String descripcion, Dificultad nivel, boolean disponible, String ruta) {
+        this.idE = id;
+        this.descripcion = descripcion;
+        this.nivel = nivel;
+        this.disponible = disponible;
+        this.ruta = ruta;
+    }
+
+    public Integer getId() {
+        return idE;
+    }
+
+    public void setId(Integer id) {
+        this.idE = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Dificultad getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Dificultad nivel) {
+        this.nivel = nivel;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    
+    
 }
