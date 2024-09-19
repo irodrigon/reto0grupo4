30537e9c3857c0e67fb7f27dae1cdb8011e4ac6f
Alin <Alin@DESKTOP-GN9K3O6>
2024-09-19 12:34:48 +0200

    Singleton y conexion a base de datos implementadas

--- Reto0Grupo4ADTi/src/controller/ControllerAL.java
+++ Reto0Grupo4ADTi/src/controller/ControllerAL.java
@@ -6,6 +6,12 @@
 package controller;
 
 import model.ConvocatoriaExamen;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Enunciado;
+
 
 /**
  *
@@ -16,8 +22,54 @@ public class ControllerAL implements InterfaceControllerAL{
     @Override
     public ConvocatoriaExamen consultarConvocatoriaEnunciado(Integer idE) {
         return null;
-        
+    }
+    
+    private Connection con;
+
+	// Sirve para gestionar las sentencias SQL.
+    private PreparedStatement stmt;
+
+    
+    private final String MUESTRA_CONVOCATORIA_ENUNCIADO_POR_ID = "SELECT convocatoria FROM CONVOCATORIAEXAMEN WHERE IDE = ?";
+
+    @Override
+    public String rutaEnunciadoPorId(Integer idE) {
+                ResultSet rs = null;
+		String convocatoria = "";
+
+		con = SingletonConnection.getConnection();
+
+		try {
+			stmt = con.prepareStatement(MUESTRA_CONVOCATORIA_ENUNCIADO_POR_ID);
+
+			// Cargamos los parámetros
+			stmt.setInt(1, idE);
+
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				
+				convocatoria = rs.getString("convocatoria");
+				
+			}else{
+                            System.out.println("No se encuentra la entrada.");
+                        }
+		} catch (SQLException e) {
+			System.out.println("Error de SQL");
+			e.printStackTrace();
+		} finally {
+			// Cerramos ResultSet
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+					System.out.println("Error en cierre del ResultSet");
+				}
+			}
+		}
+		return convocatoria;
     }
 
+    
     
 }
