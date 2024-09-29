4badad1227dc3774ddfe22f68a3b8a71f8e450be
irodrigon <75447146+irodrigon@users.noreply.github.com>
2024-09-29 16:45:21 +0200

    Update InterfaceControllerAL.java

--- Reto0Grupo4ADTi/src/controller/InterfaceControllerAL.java
+++ Reto0Grupo4ADTi/src/controller/InterfaceControllerAL.java
@@ -1,19 +1,1 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package controller;
 
-import java.util.ArrayList;
-import model.ConvocatoriaExamen;
-
-/**
- *
- * @author Alin
- */
-public interface InterfaceControllerAL {
-public ArrayList<ConvocatoriaExamen> consultarConvocatoriaEnunciado(Integer idE);
-    public ConvocatoriaExamen nuevaConvocatoria();
-    public void crearConvocatoria();
-}
